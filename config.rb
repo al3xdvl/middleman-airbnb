630ff7f772018595cd624dcce4fa11fc5e758fbd
Alex Deval <al3x.dvl@gmail.com>
2018-02-05 17:21:36 +0100

    première page dynamique

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["al3xdvl", "ssaunier", "papillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
